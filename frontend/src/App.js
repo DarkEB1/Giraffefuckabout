a6e7a176dd0bb22b650d89462779b99d0f9dfd5f
EB1 <91986545+DarkEB1@users.noreply.github.com>
2025-02-05 21:04:30 +0000

    Add link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Button } from './ui/button';
 import { Select, SelectItem } from './ui/select';
 
-const BACKEND_URL = 'https://localhost:3000'; // Update this URL
+const BACKEND_URL = 'https://giraffe-backend-h31u.onrender.com'; // Update this URL
 
 export default function App() {
   const [image, setImage] = useState(null);
